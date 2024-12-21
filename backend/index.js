8b0b8a1d6498bfe186485b5ad6e73278e32475f9
Sayan Mukherjee <mukherjeesayan8055@gmail.com>
2024-12-21 21:08:03 +0530

    origin

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-hunt-mgvf-xuevt8xzf-sayan-mukherjees-projects-a93ddbb1.vercel.app',
     credentials:true
 }
 
