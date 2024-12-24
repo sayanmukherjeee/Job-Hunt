7ef7d6c045af5673816746d74f8828f52c209c81
Sayan Mukherjee <mukherjeesayan8055@gmail.com>
2024-12-24 12:48:52 +0530

    vercel deployment2

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://https://job-hunt-topaz.vercel.app/user";
-export const JOB_API_END_POINT="https://https://job-hunt-topaz.vercel.app/job";
-export const APPLICATION_API_END_POINT="https://https://job-hunt-topaz.vercel.app/application";
-export const COMPANY_API_END_POINT="https://https://job-hunt-topaz.vercel.app/company";
+export const USER_API_END_POINT="https://job-hunt-topaz.vercel.app/user";
+export const JOB_API_END_POINT="https://job-hunt-topaz.vercel.app/job";
+export const APPLICATION_API_END_POINT="https://job-hunt-topaz.vercel.app/application";
+export const COMPANY_API_END_POINT="https://job-hunt-topaz.vercel.app/company";
